7874f23f522d7bb0c493af09b973efde102a7174
Dmitriy Sivachenko <dmitriysivachenko52@gmail.com>
2023-12-20 11:42:51 +0300

    Initial commit

---
+++ src/test/java/com/innowise/auditmicroservice/AuditMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.auditmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
