3cd2c0a37623061f1ae13e609374e69b47b8c0ec
Dmitriy Sivachenko <dmitriysivachenko52@gmail.com>
2023-12-20 12:46:05 +0300

    Was added security configuration (custom filter, userDetails class etc.)

---
+++ src/main/java/com/innowise/auditmicroservice/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.innowise.auditmicroservice.security;
+
+import com.innowise.auditmicroservice.security.filter.AuthTokenFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/**").authenticated())
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
